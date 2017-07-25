61317542213fbeea0f6d513c6d32b5f9f1718e92
princess5535 <837478514@qq.com>
2017-07-25 15:04:52 +0800

    . update source to be secure link

--- app/containers/MusicPlayer/index.js
+++ app/containers/MusicPlayer/index.js
@@ -24,16 +24,16 @@ export class MusicPlayer extends React.PureComponent { // eslint-disable-line re
         {
           name: '一次就好',
           singer: '孙露',
-          audio: 'http://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%AD%99%E9%9C%B2%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD.mp3',
-          lyric: 'http://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%AD%99%E9%9C%B2%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD.lrc',
-          thumbnail: 'http://onehower.oss-cn-shenzhen.aliyuncs.com/images/music/023b5bb5c9ea15ce8ce4435abe003af33a87b25f.jpg',
+          audio: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%AD%99%E9%9C%B2%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD.mp3',
+          lyric: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%AD%99%E9%9C%B2%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD.lrc',
+          thumbnail: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/images/music/023b5bb5c9ea15ce8ce4435abe003af33a87b25f.jpg',
         },
         {
           name: '平凡之路',
           singer: '左凡',
-          audio: 'http://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%B7%A6%E5%87%A1%20-%20%E5%B9%B3%E5%87%A1%E4%B9%8B%E8%B7%AF.mp3',
-          lyric: 'http://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%B7%A6%E5%87%A1%20-%20%E5%B9%B3%E5%87%A1%E4%B9%8B%E8%B7%AF.lrc',
-          thumbnail: 'http://onehower.oss-cn-shenzhen.aliyuncs.com/images/music/timg.jpg',
+          audio: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%B7%A6%E5%87%A1%20-%20%E5%B9%B3%E5%87%A1%E4%B9%8B%E8%B7%AF.mp3',
+          lyric: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%B7%A6%E5%87%A1%20-%20%E5%B9%B3%E5%87%A1%E4%B9%8B%E8%B7%AF.lrc',
+          thumbnail: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/images/music/timg.jpg',
         },
       ],
     };
