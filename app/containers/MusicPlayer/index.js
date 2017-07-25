b3439ceb69a9296207fb1865d6268bda9e07e4cf
princess5535 <837478514@qq.com>
2017-07-25 18:14:37 +0800

    . get song list from back end

--- app/containers/MusicPlayer/index.js
+++ app/containers/MusicPlayer/index.js
@@ -15,28 +15,26 @@ import FlexCenter from 'components/FlexCenter';
 import FlexColumnCenter from 'components/FlexColumnCenter';
 import WidgetMusicPlayer from 'components/WidgetMusicPlayer';
 
+import request from 'utils/request';
+
 export class MusicPlayer extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
 
     this.state = {
-      list: [
-        {
-          name: '一次就好',
-          singer: '孙露',
-          audio: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%AD%99%E9%9C%B2%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD.mp3',
-          lyric: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%AD%99%E9%9C%B2%20-%20%E4%B8%80%E6%AC%A1%E5%B0%B1%E5%A5%BD.lrc',
-          thumbnail: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/images/music/023b5bb5c9ea15ce8ce4435abe003af33a87b25f.jpg',
-        },
-        {
-          name: '平凡之路',
-          singer: '左凡',
-          audio: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%B7%A6%E5%87%A1%20-%20%E5%B9%B3%E5%87%A1%E4%B9%8B%E8%B7%AF.mp3',
-          lyric: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/audio/1/%E5%B7%A6%E5%87%A1%20-%20%E5%B9%B3%E5%87%A1%E4%B9%8B%E8%B7%AF.lrc',
-          thumbnail: 'https://onehower.oss-cn-shenzhen.aliyuncs.com/images/music/timg.jpg',
-        },
-      ],
+      list: [],
     };
+  }
+
+  componentDidMount() {
+    const self = this;
+
+    request.doGet('song/list')
+      .then((res) => {
+        self.setState({
+          list: res,
+        });
+      });
   }
 
   render() {
@@ -48,26 +46,29 @@ export class MusicPlayer extends React.PureComponent { // eslint-disable-line re
           <Media query="(max-width: 928px)">
             {matches => (
               <FlexCenter>
-                <FlexColumnCenter>
-                  <WidgetMusicPlayer songList={list} />
-                  <article style={{
-                    marginTop: '15px',
-                    color: 'rgba(0, 0, 0, 0.54)',
-                    textAlign: 'center',
-                    fontSize: '12px',
-                  }}>
-                    <div>基于React的一个简单的音乐播放器 ---- OneHower - Music</div>
-                    <div>v.1.0.0</div>
-                    <div style={{ textAlign: 'left' }}>
-                      Next Schedule:
-                      <ol>
-                        <li>Responsible for mobile</li>
-                        <li>Volume && Time line control</li>
-                        <li>Add more function things</li>
-                      </ol>
-                    </div>
-                  </article>
-                </FlexColumnCenter>
+                {list.length > 0 ?
+                  <FlexColumnCenter>
+                    <WidgetMusicPlayer songList={list} />
+                    <article style={{
+                      marginTop: '15px',
+                      color: 'rgba(0, 0, 0, 0.54)',
+                      textAlign: 'center',
+                      fontSize: '12px',
+                    }}>
+                      <div>基于React的一个简单的音乐播放器 ---- OneHower - Music</div>
+                      <div>v.1.0.0</div>
+                      <div style={{ textAlign: 'left' }}>
+                        Next Schedule:
+                        <ol>
+                          <li>Responsible for mobile</li>
+                          <li>Volume && Time line control</li>
+                          <li>Add more function things</li>
+                        </ol>
+                      </div>
+                    </article>
+                  </FlexColumnCenter> :
+                  <div>请加入歌单先~</div>
+                }
               </FlexCenter>
             )}
           </Media>
